f89b29f41dac2df7daefe9f8cbf99d6687fd263e
Daniel <59575049+lumijiez@users.noreply.github.com>
2024-12-25 01:27:37 +0200

    Fix nginx ports

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to reaaad tsheaas documentation</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">sveltde.dev/docs/kit</a> to reaaassd tsheaas documentation</p>
